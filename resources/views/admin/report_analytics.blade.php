8c9b0492101863d9af08001553ef8d06a4c6eef4
merrickfortis24 <fortismerrick@gmail.com>
2026-05-03 14:25:14 +0800

    fix: update header title to a static value for consistency in report analytics page

--- resources/views/admin/report_analytics.blade.php
+++ resources/views/admin/report_analytics.blade.php
@@ -8,9 +8,7 @@
 @section('sidebar_nav_aria_label', 'Main navigation')
 @section('render_default_hamburger', 'false')
 
-@section('header_title')
-    Reports {{ $header_title }} Analytics
-@endsection
+@section('header_title', 'Reports and Analytics')
 @section('header_subtitle', 'Visual charts summarizing donation trends and user activity')
 
 @section('header_slot')
