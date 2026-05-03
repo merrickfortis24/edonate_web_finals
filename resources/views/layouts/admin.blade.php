a9011087dfddb50950b47d1e6cdff262db9fcde1
merrickfortis24 <fortismerrick@gmail.com>
2026-05-03 16:46:57 +0800

    feat: integrate Vite for asset management and add admin-specific styles

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,7 +52,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @vite(['resources/css/admin.css', 'resources/js/app.js'])
     @stack('admin_head')
 </head>
 <body class="admin-page {{ $adminPageClass }}">
