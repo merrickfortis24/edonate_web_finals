b627cef6c796a854dcba2488fe01461215ff2acb
merrickfortis24 <fortismerrick@gmail.com>
2026-05-03 08:55:24 +0800

    feat: create admin login page with responsive layout and branding

--- resources/views/admin/admin_login.blade.php
+++ resources/views/admin/admin_login.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Admin Login Page 2424Merrick Nga pala to sannanajugyvygh | eDonatess</title>
+	<title>Admin Login Page 2424Merrick Nga pala to sannanajugyvygh21 | eDonatess</title>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
