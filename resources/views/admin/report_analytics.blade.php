075facb42eda6f29ad547e6c458330cbf995dbbb
merrickfortis24 <fortismerrick@gmail.com>
2026-05-03 14:21:43 +0800

    fix: update header title to include dynamic variable for report analytics page

--- resources/views/admin/report_analytics.blade.php
+++ resources/views/admin/report_analytics.blade.php
@@ -8,7 +8,9 @@
 @section('sidebar_nav_aria_label', 'Main navigation')
 @section('render_default_hamburger', 'false')
 
-@section('header_title', 'Reports & Analytics')
+@section('header_title')
+    Reports {{ $header_title }} Analytics
+@endsection
 @section('header_subtitle', 'Visual charts summarizing donation trends and user activity')
 
 @section('header_slot')
