205a1ca7bb70d75b5aede97f63f2c72f9bc14631
merrickfortis24 <fortismerrick@gmail.com>
2026-05-04 22:53:43 +0800

    feat: update cache-busting versioning for questions management script

--- resources/views/admin/eligibility/questions/index.blade.php
+++ resources/views/admin/eligibility/questions/index.blade.php
@@ -250,5 +250,5 @@
 @endsection
 
 @push('admin_scripts')
-<script src="{{ asset('js/admin/questions-management.js') }}?v={{ filemtime(public_path('js/admin/questions-management.js')) }}"></script>
+<script src="{{ asset('js/admin/questions-management.js') }}?v={{ time() }}"></script>
 @endpush
